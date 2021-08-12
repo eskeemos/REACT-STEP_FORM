aea935e12d4128292d780e78b850994fe24bb91b
Łukasz <l.michalik004@gmail.com>
2021-08-13 00:11:01 +0200

    react step form with materialUI framework

---
+++ src/components/FormUserDetails.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+
+export class FormUserDetails extends Component {
+    continue = e => {
+        e.preventDefault();
+        this.props.nextStep();
+    }
+    render() {
+        const {values} = this.props;
+        return (
+            <MuiThemeProvider>
+                <React.Fragment>
+                    <AppBar title="Enter User Details" />
+                    <TextField hintText="Enter Your First Name" 
+                        floatingLabelText="First Name"
+                        onChange={this.props.handleChange('firstName')}
+                        defaultValue={values.firstName}/>
+                        <br />
+                    <TextField hintText="Enter Your Last Name" 
+                        floatingLabelText="Last Name"
+                        onChange={this.props.handleChange('lastName')}
+                        defaultValue={values.lastName}/>
+                        <br />
+                    <TextField hintText="Enter Email" 
+                        floatingLabelText="Email"
+                        onChange={this.props.handleChange('email')}
+                        defaultValue={values.email}/>
+                        <br />
+                    <RaisedButton 
+                        label="Continue"
+                        primary="true"
+                        style={styles.button}
+                        onClick={this.continue}></RaisedButton>
+                </React.Fragment>
+            </MuiThemeProvider>
+        )
+    }
+}
+
+const styles = {
+    button: {
+        margin: 15
+    }
+}
+
+export default FormUserDetails
